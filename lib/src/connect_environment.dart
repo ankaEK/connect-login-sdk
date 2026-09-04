b07aea85b118426e10d5c94c150958bc2463e0ec
Adarsha Manandhar <adarshamdhr@gmail.com>
2026-09-04 15:13:57 +0545

    Dfix:

--- lib/src/connect_environment.dart
+++ lib/src/connect_environment.dart
@@ -22,10 +22,6 @@ enum ConnectEnvironment {
   Uri get authorizeBaseUri =>
       Uri.parse('$authHost/api/v1/oauth/authorize');
 
-  /// Custom URL scheme registered by the matching Connect Persona app build.
-  ///
-  /// Hosts must declare these in Android `<queries>` /
-  /// iOS `LSApplicationQueriesSchemes` so `canLaunchUrl` can detect the app.
   String get connectAppScheme {
     switch (this) {
       case ConnectEnvironment.dev:
@@ -37,7 +33,6 @@ enum ConnectEnvironment {
     }
   }
 
-  /// Native Connect app authorize URI for this environment.
   Uri get connectAppAuthorizeUri =>
       Uri(scheme: connectAppScheme, host: 'oauth', path: '/authorize');
 }
